1cd28a7b84f855c8034469f5a04f309df7713c60
Jonas94 <jonas.fredriksson94@gmail.com>
2018-12-01 22:46:45 +0100

    Solved day 1 part 2.

--- src/aoc/Day1.java
+++ src/aoc/Day1.java
@@ -3,28 +3,55 @@ package aoc;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day1 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		
+		List<Integer> frequencies = readFileIntoList();
+		List<Integer> allResultingFrequencies = new ArrayList<Integer>();
+		int resultingFrequency = 0;
+		boolean duplicateFrequencyFound = false;
+		
+		while (!duplicateFrequencyFound) {
+			//Takes a while... about 145k iterations with the input from day 1.
+			for (Integer currentFrequency : frequencies) {
+				resultingFrequency += currentFrequency;
+				
+				if (!allResultingFrequencies.contains(resultingFrequency)) {
+					allResultingFrequencies.add(resultingFrequency);
+				}
+				else {
+					duplicateFrequencyFound = true;
+					break;
+				}
+			}
+		}
+			System.out.println("First duplicated frequency: " + resultingFrequency);
+	}
 
-        int resultingFrequency = 0;
-        int currentFrequency = 0;
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader("resources/inputDay1.txt"));
-            String line = reader.readLine();
-            while (line != null) {
-                currentFrequency = Integer.parseInt(line);
-                resultingFrequency += currentFrequency;
-                line = reader.readLine();
-            }
-            reader.close();
+	private static List<Integer> readFileIntoList() {
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		int currentFrequency = 0;
+		List<Integer> frequencies = new ArrayList<Integer>();
 
-        System.out.println("Resulting frequency: " + resultingFrequency);
-    }
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader("resources/inputDay1.txt"));
+			String line = reader.readLine();
+			while (line != null) {
+				currentFrequency = Integer.parseInt(line);
+				frequencies.add(currentFrequency);
+				line = reader.readLine();
+			}
+			reader.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return frequencies;
+	}
 }
