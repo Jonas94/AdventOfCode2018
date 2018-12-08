c96f57eb522f1a87cafe5fa459446e9ee3566bf2
Jonas94 <jonas.fredriksson94@gmail.com>
2018-12-08 13:36:08 +0100

    cleanup of SleepyGuard class.

--- src/aoc/SleepyGuard.java
+++ src/aoc/SleepyGuard.java
@@ -6,15 +6,12 @@ import java.util.TreeMap;
 
 public class SleepyGuard {
 
-    private int id;
     Map<Integer, Integer> minuteMap = new HashMap<Integer, Integer>();
-
+    private int id;
 
     public void addSleepingMinutes(int[] minutes) {
-
         for (int minute : minutes) {
             minuteMap.merge(minute, 1, Integer::sum);
-
         }
     }
 
@@ -35,15 +32,13 @@ public class SleepyGuard {
     public int getTotalSleepingMinutes() {
         int minuteCount = 0;
         for (int minute : minuteMap.keySet()) {
-         //   System.out.println(minute + "  " + minuteMap.get(minute));
             minuteCount += minuteMap.get(minute);
         }
-
         return minuteCount;
     }
 
 
-    public TreeMap getMostSleptMinute(){
+    public TreeMap getMostSleptMinute() {
         int minuteMostSlept = 0;
         int minuteMostSleptCount = 0;
         TreeMap<Integer, Integer> mostSleptMinute = new TreeMap<>();
@@ -52,13 +47,9 @@ public class SleepyGuard {
             if (minuteMap.get(keyMinute) > minuteMostSleptCount) {
                 minuteMostSlept = keyMinute;
                 minuteMostSleptCount = minuteMap.get(keyMinute);
-
             }
         }
         mostSleptMinute.put(minuteMostSlept, minuteMostSleptCount);
         return mostSleptMinute;
     }
-
-
-
 }
